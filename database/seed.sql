2ac14099b730c3482a4f00c973c44bbb90f06ad3
Jean TSHIKAKU <jtshikaku@gmail.com>
2026-08-07 12:30:28 +0200

    Mise à jour du dépôt distant -

--- database/seed.sql
+++ database/seed.sql
@@ -13,12 +13,14 @@ DELETE FROM eleves;
 DELETE FROM classes;
 DELETE FROM options_secondaire;
 DELETE FROM institutions;
+DELETE FROM administrateurs;
 ALTER TABLE eleves AUTO_INCREMENT = 1;
 ALTER TABLE classes AUTO_INCREMENT = 1;
 ALTER TABLE options_secondaire AUTO_INCREMENT = 1;
 ALTER TABLE institutions AUTO_INCREMENT = 1;
 ALTER TABLE presences AUTO_INCREMENT = 1;
 ALTER TABLE responsables AUTO_INCREMENT = 1;
+ALTER TABLE administrateurs AUTO_INCREMENT = 1;
 SET FOREIGN_KEY_CHECKS = 1;
 
 -- --------------------------------------------------------
