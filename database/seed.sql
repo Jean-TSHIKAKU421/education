51b487f3b1e7289655f771762ccb45984431b7b1
Jean TSHIKAKU <jtshikaku@gmail.com>
2026-08-09 21:06:21 +0200

    Mise à jour - 2026-08-09 21:06:21

--- database/seed.sql
+++ database/seed.sql
@@ -17,7 +17,5 @@ ALTER TABLE responsables AUTO_INCREMENT = 1;
 ALTER TABLE administrateurs AUTO_INCREMENT = 1;
 SET FOREIGN_KEY_CHECKS = 1;
 
--- Déchargement des données de la table `administrateurs`
-
-INSERT INTO `administrateurs` (`id`, `username`, `password_hash`, `nom_complet`, `role`) VALUES
-(1, 'admin', '$2b$10$YuwYOrLoQmAzbmq.8hNhu.9VlfZW2K76ky6JjvYcd3kj8j9VcBOH6', 'Administrateur Principal', 'super_admin');
+INSERT INTO `administrateurs` (`username`, `password_hash`, `nom_complet`, `role`) VALUES
+('super-admin', '$2b$10$FBePQl7VckkLM6fGMRq5E.rpr01yXpsFEHXpf9cB.teUp.KWfqj1a', 'Jean TSHIKAKU', 'super_admin');
